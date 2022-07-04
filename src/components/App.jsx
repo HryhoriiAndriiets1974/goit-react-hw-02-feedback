07f8435e9289ec5e98fe98d55708aa709f53f9e4
HryhoriiAndriiets1974 <ander_1_agg@ukr.net>
2022-07-05 00:41:07 +0300

    task-js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,57 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Component } from "react";
+import Section from "./Section";
+import Statistics from "./Statistics";
+import FeedbackOptions from "./FeedbackOptions";
+import Notification from "./Notification/Notification";
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+countTotalFeedback() {
+  const {good, neutral, bad} = this.state;
+  return good + neutral + bad;
 };
+
+countPositiveFeedbackPercentage() {
+  return Math.round((this.state.good / this.countTotalFeedback()*100));
+};
+
+onLeaveFeedback = e => {
+  this.setState(prevState => (
+    {[e]: prevState[e] + 1}
+  ))
+};
+
+  render () {
+    const {good, neutral, bad} = this.state;
+    const options = Object.keys(this.state);
+    return (
+    <>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={options}
+          onLeaveFeedback={this.onLeaveFeedback}
+        />
+      </Section>
+      <Section title="Statistics">
+        {this.countTotalFeedback() > 0
+        ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={this.countTotalFeedback()}
+            positivPercentage={this.countPositiveFeedbackPercentage()}
+          />)
+        : (<Notification message="There is no feedback" />)
+        }
+      </Section>
+    </>
+  );}
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,51 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import App from 'components/App';
 import './index.css';
-
-class Counter extends Component {
-  static defaultProps = {
-    step: 10,
-    initialValue: 0,
-  };
-
-  state = {
-    value: this.props.initialValue,
-  };
-
-  handleIncrement = () => {
-    this.setState((state, props) => ({
-        value: state.value + props.step,
-    }))
-  };
-
-
-  handleDecrement = () => {
-    this.setState((state, props) => ({
-        value: state.value - props.step,
-    }))
-  };
-
-  render() {
-    const { step } = this.props;
-
-    return (
-      <div>
-        <span>{this.state.value}</span>
-        <button type="button" onClick={this.handleIncrement}>
-          Increment by {step}
-        </button>
-        <button type="button" onClick={this.handleDecrement}>
-          Decrement by {step}
-        </button>
-      </div>
-    );
-  }
-}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Counter />
     <App />
   </React.StrictMode>
 );
